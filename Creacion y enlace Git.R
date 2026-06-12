2d5fc342efb640e4857430423229834d659cdacb
guillermocoronado <guillermo.coronado@pucp.edu.pe>
2026-06-12 16:46:04 -0500

    Initial commit

---
+++ Creacion y enlace Git.R
@@ -0,0 +1,17 @@
+#===========================================================================================
+#Proyecto: Análisis de la informalidad laboral usando datos de la ENAHO
+#Autor: Guillermo Coronado
+#Objetivo de este script: Crear el sistema de carpetas y enlazar con GitHub
+#Fecha: 12-06-2026
+#==========================================================================================
+
+#Creamos carpetas--------------------------
+
+dir.create("datos")
+dir.create("datos/crudos")
+dir.create("datos/procesados")
+dir.create("outputs")
+dir.create("docs")
+
+
+#Enlace con Git y GitHub
