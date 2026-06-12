86b80b4662f9b1fcf17c2f0e96714ae739d483dc
guillermocoronado <guillermo.coronado@pucp.edu.pe>
2026-06-12 17:14:58 -0500

    Creamos script para carga y unión de módulos de ENAHO

---
+++ scripts/01_Carga_union_modulos.R
@@ -0,0 +1,12 @@
+#================================================================================
+#Proyecto: Análisis de la informalidad laboral usando datos de la ENAHO
+#Script: Cargar los módulos y hacer los joins
+#Autor: Guillermo Coronado
+#Fecha: 12-06-2026
+#===============================================================================
+
+#1.Carga de librerías---------------------------
+library(rio)
+library(tidyverse)
+library(janitor)
+library(readr)
