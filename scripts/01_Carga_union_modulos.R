a4ad52ef21c1de8d1f58c6e7fd88c6d123006165
guillermocoronado <guillermo.coronado@pucp.edu.pe>
2026-06-12 17:31:08 -0500

    Importamos módulos de salud, educación, empleo e ingresos, y gobernabilidad de la ENAHO

--- scripts/01_Carga_union_modulos.R
+++ scripts/01_Carga_union_modulos.R
@@ -10,3 +10,12 @@ library(rio)
 library(tidyverse)
 library(janitor)
 library(readr)
+renv::snapshot()
+
+#2. Importar datos--------------------
+mod300 <- import("datos/crudos/Enaho01A-2025-300.csv", encoding = "Latin-1")
+mod400 <- import("datos/crudos/Enaho01A-2025-400.csv", encoding = "Latin-1")
+mod500 <- import("datos/crudos/Enaho01A-2025-500.csv", encoding = "Latin-1")
+mod_gob <- import("datos/crudos/Enaho01B-2025-1.csv", encoding = "Latin-1")
+
+#3.
